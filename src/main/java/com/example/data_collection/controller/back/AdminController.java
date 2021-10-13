176bfebb04bed5e0f115ab682ccaad04fc5903a7
yangxiuwei123 <3140768737@qq.com>
2021-10-14 00:03:08 +0800

    Merge remote-tracking branch 'origin/master' into master
    # Conflicts:
    #	src/main/java/com/example/data_collection/controller/back/AdminController.java
    #	src/main/java/com/example/data_collection/controller/back/StationController.java
    #	src/main/java/com/example/data_collection/controller/back/StudentController.java
    #	src/main/java/com/example/data_collection/controller/back/StudentStationController.java
    #	src/main/java/com/example/data_collection/dao/StationDao.java
    #	src/main/java/com/example/data_collection/dao/StudentDao.java
    #	src/main/java/com/example/data_collection/dao/StudentStationDao.java
    #	src/main/java/com/example/data_collection/service/AdminService.java
    #	src/main/java/com/example/data_collection/service/StationService.java
    #	src/main/java/com/example/data_collection/service/StudentService.java
    #	src/main/java/com/example/data_collection/service/StudentStationService.java
    #	src/main/java/com/example/data_collection/service/impl/AdminServiceImpl.java
    #	src/main/java/com/example/data_collection/service/impl/StationServiceImpl.java
    #	src/main/java/com/example/data_collection/service/impl/StudentServiceImpl.java
    #	src/main/java/com/example/data_collection/service/impl/StudentStationServiceImpl.java

--- src/main/java/com/example/data_collection/controller/back/AdminController.java
+++ src/main/java/com/example/data_collection/controller/back/AdminController.java
@@ -10,16 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-<<<<<<< HEAD
 import javax.websocket.server.PathParam;
-=======
->>>>>>> origin/master
 import java.util.List;
 
 @RestController
 @RequestMapping("/back/admin")
 public class AdminController {
-<<<<<<< HEAD
     //注入
     @Resource
     private AdminService adminService;
@@ -28,57 +24,41 @@ public class AdminController {
      * 查询所有管理员信息
      * @return
      */
-=======
-
-    @Resource
-    private AdminService adminService;
-
->>>>>>> origin/master
     @RequestMapping("/findAll")
     public ResponseResult getAllAdmin(){
         return adminService.findAll();
     }
 
-<<<<<<< HEAD
     /**
      * 通过id查询管理员
      * @param id
      * @return
      */
-=======
->>>>>>> origin/master
     @RequestMapping("/findById/{id}")
     public ResponseResult getAdminById(@PathVariable("id") Long id){
         return adminService.findById(id);
     }
 
-<<<<<<< HEAD
     /**
      * 添加管理员
      * @param admin
      * @return
      */
-=======
->>>>>>> origin/master
     @RequestMapping("/addAdmin")
     public ResponseResult addAdmin(Admin admin){
         return adminService.insertAdmin(admin);
     }
 
-<<<<<<< HEAD
     /**
      * 修改管理员信息
      * @param admin
      * @return
      */
-=======
->>>>>>> origin/master
     @RequestMapping("/updateAdmin")
     public ResponseResult updateAdmin(Admin admin){
         return adminService.updateAdmin(admin);
     }
 
-<<<<<<< HEAD
     /**
      * 通过id删除管理员
      * @param id
@@ -88,10 +68,4 @@ public class AdminController {
     public ResponseResult deleteAdminById(@PathParam("id") Long id){
         return adminService.deleteAdminById(id);
     }
-=======
-//    @RequestMapping("/deleteAdmin/{id}")
-//    public ResponseResult deleteAdminById(@PathVariable("id") Long id){
-//        return adminService.deleteAdminById(id);
-//    }
->>>>>>> origin/master
 }
--- src/main/java/com/example/data_collection/dao/StudentStationDao.java
+++ src/main/java/com/example/data_collection/dao/StudentStationDao.java
@@ -3,7 +3,16 @@ package com.example.data_collection.dao;
 import com.example.data_collection.entity.StudentStation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface StudentStationDao extends JpaRepository<StudentStation , Long>,
         JpaSpecificationExecutor<StudentStation> {
+    //关联查询学生信息，岗位信息，部门信息，公司、选择时间信息
+    @Query(value = "select s_number, s_college, s_department, s_class, s_name,s_phone, c_name, st_name, ss_time from t_student_station left join t_student on t_student_station.s_id = t_student.s_id left join t_station on t_student_station.st_id=t_station.st_id LEFT  JOIN t_company on t_company.c_id=t_station.c_id",nativeQuery = true)
+    List<Object[]> findAllSs();
+    //通过班级进行关联查询
+    @Query(value = "select s_number, s_college, s_department, s_class, s_name,s_phone, c_name, st_name, ss_time from t_student_station left join t_student on t_student_station.s_id = t_student.s_id left join t_station on t_student_station.st_id=t_station.st_id LEFT  JOIN t_company on t_company.c_id=t_station.c_id where s_class like %?1%",nativeQuery = true)
+    List<Object[]> findBySClass(String sClass);
 }
