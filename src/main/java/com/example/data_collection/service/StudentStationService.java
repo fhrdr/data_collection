882ba4c3fac90434d9ed938e22f0bc7ca512a300
fhrdr <1837211345@qq.com>
2021-10-14 10:36:13 +0800

    修正部分bug

--- src/main/java/com/example/data_collection/service/StudentStationService.java
+++ src/main/java/com/example/data_collection/service/StudentStationService.java
@@ -1,17 +1,18 @@
 package com.example.data_collection.service;
 
 import com.example.data_collection.entity.StudentStation;
-import com.example.data_collection.entity.StudentStationInfo;
 import com.example.data_collection.result.ResponseResult;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
 public interface StudentStationService {
     // 学生选择岗位
-    ResponseResult studentChooseStation(Long stId , HttpSession session);
+    ResponseResult studentChooseStation(Long stId , HttpServletRequest request);
     // 学生取消岗位
-    ResponseResult studentCancelStation(Long stId , HttpSession session);
+    ResponseResult studentCancelStation(Long stId , HttpServletRequest request);
 
     // 杨修伟部分
     /**
@@ -20,6 +21,8 @@ public interface StudentStationService {
      * @return
      */
     ResponseResult insertStudentStation(StudentStation studentStation);
-
-    List<StudentStationInfo> findStudentinfo();
+    //  查询学生姓名，部门，班级，公司，岗位，选择时间信息
+    ResponseResult findStudentinfo();
+    //通过班级查询学生，岗位，公司的相关信息
+    ResponseResult findBySclass(String sClass);
 }
